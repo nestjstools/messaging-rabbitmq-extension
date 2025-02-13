8d77beb919fe0b7ae0e10d158b2464326e8b26f9
sebastian <sebastian.iwanczyszyn@gmail.com>
2025-02-13 22:57:26 +0100

    Define module as global

--- src/messaging-rabbitmq-extension.module.ts
+++ src/messaging-rabbitmq-extension.module.ts
@@ -1,9 +1,10 @@
-import { Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
 import { AmqpMessageBusFactory } from './message-bus/amqp-message-bus-factory';
 import { AmqpChannelFactory } from './channel/amqp-channel.factory';
 import { RabbitmqMessagingConsumer } from './consumer/rabbitmq-messaging.consumer';
 import { RabbitmqMigrator } from './migrator/rabbitmq.migrator';
 
+@Global()
 @Module({
   providers: [
     RabbitmqMigrator,
