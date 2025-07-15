dfa193f3c5a736fc66def2dfc6fb047bf3c1efd3
sebastian <sebastian.iwanczyszyn@gmail.com>
2025-07-15 15:13:57 +0200

    feat: close connection in correct way

--- src/messaging-rabbitmq-extension.module.ts
+++ src/messaging-rabbitmq-extension.module.ts
@@ -3,7 +3,7 @@ import { AmqpMessageBusFactory } from './message-bus/amqp-message-bus-factory';
 import { AmqpChannelFactory } from './channel/amqp-channel.factory';
 import { RabbitmqMessagingConsumer } from './consumer/rabbitmq-messaging.consumer';
 import { RabbitmqMigrator } from './migrator/rabbitmq.migrator';
-import { RmqChannelFactory } from './channel/rmq-channel-factory.service';
+import { RmqChannelFactory } from './channel/rmq-channel-factory';
 
 @Global()
 @Module({
