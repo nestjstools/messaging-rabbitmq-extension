bc348b68082ae350d8cb0b22acd7ae973bc6ab46
sebastian <sebastian.iwanczyszyn@gmail.com>
2026-02-26 11:43:21 +0100

    feat!: bump

--- src/consumer/rabbitmq-messaging.consumer.ts
+++ src/consumer/rabbitmq-messaging.consumer.ts
@@ -46,6 +46,7 @@ export class RabbitmqMessagingConsumer
     this.amqpChannel = channelWrapper;
 
     await channelWrapper.addSetup(async (rawChannel: Channel) => {
+      await rawChannel.prefetch(1);
       return rawChannel.consume(
         channel.config.queue,
         async (msg: ConsumeMessage | null) => {
